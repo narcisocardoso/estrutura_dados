668586a13ae2a6883862826e09b242460ee1825a
cardoso <narcisocardosobsi@gmail.com>
2024-04-14 12:39:43 -0300

    Criancao da secao 06

---
+++ cursoKotlinUdemyDevMasterTeam/Seção 05/src/main/kotlin/aula23/Main.kt
@@ -0,0 +1,24 @@
+package aula23
+
+fun main () {
+    println("Aula 23") // operadores artimeticos
+
+    var idade = 30
+//    idade = idade + 1
+//    idade = idade -5
+//    idade = idade *3
+//    idade = idade % 3
+//    idade = idade /2
+
+
+    idade += 1
+    idade -= 5
+    idade *= 3
+    idade /= 2
+
+    println(idade)
+     idade++
+    idade--
+    println(idade)
+
+}
---
+++ cursoKotlinUdemyDevMasterTeam/Seção 05/src/main/kotlin/aula24/Main.kt
@@ -0,0 +1,20 @@
+package aula24
+
+fun main() {
+    println("Aula 24")
+
+
+    var nomeCompleto: String = "Narciso Rocha"
+    var texto: String
+
+    val numeroSapato: Byte = 45
+
+    val PIB: Long = 1869000000000
+
+    val populacao: Int = 211000000
+
+
+    println("O PIB per capta é: ${PIB / populacao}")
+
+
+}
